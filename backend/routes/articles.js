901bc13145a679c1e8d73a113bc040b72edb4817
Leon <leonndp@gmail.com>
2019-10-22 18:37:02 +0800

    Added REST routes

---
+++ backend/routes/articles.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+let Article = require('../models/article.model');
+
+// Index
+router.route('/').get((req, res) => {
+	Article.find()
+		.then(articles => res.json(articles))
+		.catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+// Create
+router.route('/add').post((req, res) => {
+	const title = req.body.title;
+	const date = Date.parse(req.body.date);
+	const image = req.body.image;
+	const content = req.body.content;
+
+	const newArticle = new Article({
+		title,
+		date,
+		image,
+		content
+	})
+
+	newArticle.save()
+		.then(() => res.json('Article added!'))
+		.catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+// Read
+router.route('/:id').get((req, res) => {
+	Article.findById(req.params.id)
+		.then(article => res.json(article))
+		.catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+// Update
+router.route('/:id').put((req, res) => {
+	Article.findById(req.params.id)
+		.then(article => {
+			article.title = req.body.title;
+			article.date = Date.parse(req.body.date);
+			article.image = req.body.image;
+			article.content = req.body.content;
+
+			article.save()
+				.then(() => res.json('Article updated!'))
+				.catch(err => res.status(400).json(`Error: ${err}`))
+		})
+})
+
+// Destroy
+router.route('/:id').delete((req, res) => {
+	Article.findByIdAndDelete(req.params.id)
+	.then(() => res.json('Article deleted.'))
+	.catch(err => res.status(400).json(`Error: ${err}`))
+})
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const connection = mongoose.connection;
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
+const articlesRouter = require('./routes/articles');
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +16,8 @@ mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedT
 connection.once('open', () => {
 	console.log("MongoDB database connection established successfully");
 })
+
+app.use('/articles', articlesRouter);
 
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
