110c1a9bd91f52a0ff34c727ad916712fc09dea9
Leon <leonndp@gmail.com>
2019-10-22 21:33:59 +0800

    App.js edited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import ArticlesList from './components/ArticlesList';
+import NewArticle from './components/NewArticle';
+import EditArticle from './components/EditArticle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={ArticlesList} />
+        <Route path="/new" exact component={NewArticle} />
+        <Route path="/:id/edit" exact component={EditArticle} />
+      </div>
   );
 }
 
