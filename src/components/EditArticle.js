4069424a6d9ccf710a87bf2b44f713b5b6d9aebc
Leon <leonndp@gmail.com>
2019-10-23 12:14:12 +0800

    Finished EditArticle component

--- src/components/EditArticle.js
+++ src/components/EditArticle.js
@@ -1,9 +1,111 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class EditArticle extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.onChangeTitle = this.onChangeTitle.bind(this);
+		this.onChangeImage = this.onChangeImage.bind(this);
+		this.onChangeContent = this.onChangeContent.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+
+		this.state = {
+			title:'',
+			date:new Date(),
+			image:'',
+			content:''
+		}
+	}
+
+	componentDidMount(){
+		console.log(this.props.match.params);
+		axios.get(`http://localhost:5000/articles/${this.props.match.params.id}`)
+			.then(res => {
+				this.setState({
+					title: res.data.title,
+					date: new Date(res.data.date),
+					image: res.data.image,
+					content: res.data.content
+				})
+			})
+			.catch(err => {
+				console.log(err);
+			})
+	}
+
+	onChangeTitle(e) {
+		this.setState({
+			title: e.target.value
+		});
+	}
+
+	onChangeImage(e) {
+		this.setState({
+			image: e.target.value
+		});
+	}
+	
+	onChangeContent(e) {
+		this.setState({
+			content: e.target.value
+		});
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+
+		const article = {
+			title: this.state.title,
+			date: this.state.date,
+			image: this.state.image,
+			content: this.state.content
+		}
+
+		console.log(article);
+
+		axios.put(`http://localhost:5000/articles/${this.props.match.params.id}`, article)
+			.then(res => console.log(res.data));
+
+		window.location = `/${this.props.match.params.id}`;
+	}
+
 	render() {
 		return (
-			<div>This is the EditArticle component!</div>
+			<div>
+				<h1>Edit Post</h1>
+				<form onSubmit={this.onSubmit}>
+					<div>
+						<label>Title: </label>
+						<input 
+							type='text' 
+							value={this.state.title} 
+							onChange={this.onChangeTitle}
+							required
+							/>
+					</div>
+					<div>
+						<label>Image: </label>
+						<input 
+							type='text' 
+							value={this.state.image}
+							onChange={this.onChangeImage}
+							/>
+					</div>
+					<div>
+						<label>Content: </label>
+						<input 
+							type='textarea' 
+							value={this.state.content}
+							onChange={this.onChangeContent}
+							required
+							/>
+					</div>
+					<div>
+						<input type="submit" value="Edit Post" />
+					</div>
+				</form>
+			</div>
 		)
 	}
 }
--- src/components/ViewArticle.js
+++ src/components/ViewArticle.js
@@ -21,7 +21,7 @@ export default class ViewArticle extends React.Component {
 			.then(res => {
 				this.setState({
 					title: res.data.title,
-					date: new Date(res.data.date).toString(),
+					date: new Date(res.data.date),
 					image: res.data.image,
 					content: res.data.content
 				})
@@ -44,7 +44,7 @@ export default class ViewArticle extends React.Component {
 		return (
 			<div>
 				<h1>{this.state.title}</h1>
-				<p>{this.state.date}</p>
+				<p>{this.state.date.toString()}</p>
 				<img src={this.state.image} />
 				<p>{this.state.content}</p>
 				<Link to={this.props.match.params.id+"/edit"}>Update Article</Link>
