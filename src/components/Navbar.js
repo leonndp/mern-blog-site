5c1823e1fefee782824d9545356bca5e6d198d72
Leon <leonndp@gmail.com>
2019-10-24 14:46:25 +0800

    Styled responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ export default class Navbar extends React.Component {
 	render() {
 		return (
 			<div className="navbar">
-				<Link to="/">Blog Site</Link>
-				<Link to="/">Home</Link>
-				<Link to="/new">New Post</Link>
+				<Link className="navbar__link navbar__link--header" to="/"><strong>&#60;/&#62; Blog Site</strong></Link>
+				<Link className="navbar__link" to="/">Home</Link>
+				<Link className="navbar__link" to="/new">New Post</Link>
 			</div>
 		)
 	}
