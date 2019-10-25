3ac2ebe8400cfd06ee11b86095499eb723decb9a
Leon <leonndp@gmail.com>
2019-10-25 10:58:53 +0800

    Styled 404 page and fixed image scaling

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
-	<div>
-		404 - <Link to='/'>Go home</Link>
+	<div className="wrapper">
+		<div className="not-found">
+			<h1 className="not-found__error-header">404</h1>
+			<h1 className="not-found__error-subheader">Not Found</h1>
+			<Link className="btn btn--purple" to='/'>Go home</Link>
+		</div>
 	</div>
 );
 
