678849905d2e1e8ec77b3ba06914c219079da173
Leon <leonndp@gmail.com>
2019-10-25 13:05:45 +0800

    Fixed text wrapping

--- src/components/Article.js
+++ src/components/Article.js
@@ -9,7 +9,7 @@ const Article = props => (
 		<div className="article-link__body">
 			<h3 className="article-link__title">{props.article.title}</h3>
 			<p className="article-link__date">{moment(props.article.createdAt.substring(0,10)).format("MMM DD YYYY")}</p>
-			<p className="article-link__content">{props.article.content.length > 100 ? props.article.content.substring(0,100)+'...' : props.article.content}</p>
+			<p className="article-link__content">{props.article.content.length > 50 ? props.article.content.substring(0,50)+'...' : props.article.content}</p>
 			<Link className="btn btn--purple" to={'/view/'+props.article._id}>Read More</Link>
 		</div>
 	</div>
