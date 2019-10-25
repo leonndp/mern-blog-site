7eacd8d55eb6d0269c4c341b36bfa28892fc5be4
Leon <leonndp@gmail.com>
2019-10-25 22:18:04 +0800

    Band-aid fix attempt 3, fixing cors

--- client/src/components/ArticlesList.js
+++ client/src/components/ArticlesList.js
@@ -10,7 +10,7 @@ export default class ArticlesList extends React.Component {
 	}
 
 	componentDidMount() {
-		axios.get(`${window.location.hostname}:5000/articles/`)
+		axios.get(`https://${window.location.hostname}:5000/articles/`)
 			.then(res => {
 				this.setState({ 
 					articles: res.data.sort((a,b) => {
