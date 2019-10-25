0a45b1b8167bedf10cb9cf15844b6abd05d373e6
Leon <leonndp@gmail.com>
2019-10-25 13:29:42 +0800

    Fixed text wrapping for reals

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
+const charLimit = 100;
+
 const Article = props => (
 	<div className="article-link">
 		<img className="article-link__thumbnail" src={props.article.image} />
@@ -9,7 +11,7 @@ const Article = props => (
 		<div className="article-link__body">
 			<h3 className="article-link__title">{props.article.title}</h3>
 			<p className="article-link__date">{moment(props.article.createdAt.substring(0,10)).format("MMM DD YYYY")}</p>
-			<p className="article-link__content">{props.article.content.length > 50 ? props.article.content.substring(0,50)+'...' : props.article.content}</p>
+			<p className="article-link__content">{props.article.content.length > charLimit ? props.article.content.substring(0,charLimit)+'...' : props.article.content}</p>
 			<Link className="btn btn--purple" to={'/view/'+props.article._id}>Read More</Link>
 		</div>
 	</div>
